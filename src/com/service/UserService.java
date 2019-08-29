97e489ed9f140b32981af075c094d2d6828f9375
myyd <myyd9787@gmail.com>
2019-08-29 15:28:51 +0800

    edit传数据并显示到前端

--- src/com/service/UserService.java
+++ src/com/service/UserService.java
@@ -4,10 +4,11 @@ import com.entity.Receipt;
 import com.entity.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserService {
     //登录
     User login(String username, String pwd);
     //填写接货单goodsBillCode
-    Receipt getReceiptByGoodsBillCode(String goodsBillCode);
+    List<Receipt> getReceiptByGoodsBillCode(String goodsBillCode);
 }
